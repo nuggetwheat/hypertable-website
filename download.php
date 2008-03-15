6410e3ba20b81e59f1d50d605252cc39d5c59fd6
Luke Lu <hypertable@vicaya.com>
2008-03-14 17:25:19 -0700

    Use download.php to track downloads better

--- download.php
+++ download.php
@@ -1,4 +1,3 @@
 <?php
-$v = htmlspecialchars($_REQUEST['v']);
-$v0 = htmlspecialchars(preg_replace('/-\w+$/', '', $v));
-header("Location: http://hypertable.googlecode.com/svn/tags/v$v0/hypertable-$v-src.tar.gz");
+$v = urlencode($_REQUEST['v']);
+header("Location: http://hypertable.googlecode.com/files/hypertable-$v-src.tar.gz");

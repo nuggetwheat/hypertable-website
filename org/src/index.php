cfaeb640a9d15d9c20da8deaddf83bcc64b9c22b
Doug Judd <doug@hypertable.com>
2010-03-10 19:21:54 -0800

    Added Google analytics

--- org/src/index.php
+++ org/src/index.php
@@ -61,5 +61,14 @@
 	</div><!-- contents-container -->
 </div><!-- all -->
 <? include("footer.inc") ?>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-15179427-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
 </body>
 </html>
